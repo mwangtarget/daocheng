127a93ecd8abefc319a0a22c4141603481dbe88c
tjcuwang@yahoo.com <tjcuwang@yahoo.com>
2015-12-29 22:09:24 +0800

    Add more topics, initialset up for the batch framework

---
+++ technology-choice/batch-framework/spring-batch/src/main/java/com/daocheng/technology_choose/batch/spring_batch/App.java
@@ -0,0 +1,13 @@
+package com.daocheng.technology_choose.batch.spring_batch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
