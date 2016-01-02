f26dfbccd17604b359d97287f42fc2e1a0724a36
tjcuwang@yahoo.com <tjcuwang@yahoo.com>
2016-01-02 23:25:39 +0800

    Add XA code, not working

--- technology-choice/transaction-framework/spring-atomikos/src/main/java/com/daocheng/technology_choose/tm/spring_atomikos/XATransactionSpring.java
+++ technology-choice/transaction-framework/spring-atomikos/src/main/java/com/daocheng/technology_choose/tm/spring_atomikos/XATransactionSpring.java
@@ -12,11 +12,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-
-public class DeclaritiveTransactionSpring {
+public class XATransactionSpring {
 
 	final static Logger logger = LoggerFactory.getLogger(DeclaritiveTransactionSpring.class);
 
@@ -24,11 +22,12 @@ public class DeclaritiveTransactionSpring {
 
 		logger.info("Initializing Spring context.");
 
-		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("/context_all.xml");
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("/context_xa_all.xml");
 
 		JdbcTemplate jdbcTemplate = (JdbcTemplate) applicationContext.getBean("jdbcTemplate");
+		JdbcTemplate jdbcTemplate2 = (JdbcTemplate) applicationContext.getBean("jdbcTemplate2");
 		try {
-			saveMarketData(jdbcTemplate);
+			saveMarketData(jdbcTemplate, jdbcTemplate2);
 		} catch (Exception e) {
 			// Only to Test the rollback scenarios.
 			e.printStackTrace();
@@ -39,19 +38,36 @@ public class DeclaritiveTransactionSpring {
 			Connection con = ((DataSource) applicationContext.getBean("dataSource")).getConnection();
 			ResultSet rs = con.createStatement().executeQuery("select count(*) from MARKET_DATA_EOD_TX");
 			while (rs.next()) {
-				logger.info("Number of records in MARKET_DATA_EOD_TX:" + rs.getString(1));
+				logger.info("Number of records in MARKET_DATA_EOD_TX of H2_First:" + rs.getString(1));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			Connection con = ((DataSource) applicationContext.getBean("dataSource2")).getConnection();
+			ResultSet rs = con.createStatement().executeQuery("select count(*) from MARKET_DATA_EOD_TX");
+			while (rs.next()) {
+				logger.info("Number of records in MARKET_DATA_EOD_TX of H2_Second:" + rs.getString(1));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-    // Seems add transactional annotation is not working
+
+	// Seems add transactional annotation is not working
 	@Transactional
-	public static void saveMarketData(JdbcTemplate jdbcTemplate) {
+	public static void saveMarketData(JdbcTemplate jdbcTemplate, JdbcTemplate jdbcTemplateHSQL) {
 		jdbcTemplate.execute(format("insert into MARKET_DATA_EOD_TX (instrument, price) values('%s', %s)", "USD/INR",
 				Double.toString(20.5)));
 		jdbcTemplate.execute(format("insert into MARKET_DATA_EOD_TX (instrument, price) values('%s', %s)", "USD/SGD",
 				Double.toString(2.08)));
+		
+		jdbcTemplateHSQL.execute(format("insert into MARKET_DATA_EOD_TX (instrument, price) values('%s', %s)", "USD/INR",
+				Double.toString(20.5)));
+		jdbcTemplateHSQL.execute(format("insert into MARKET_DATA_EOD_TX (instrument, price) values('%s', %s)", "USD/SGD",
+				Double.toString(2.08)));
+	
 		if (true)
 			throw new RuntimeException("Throw Exception");
 	}
--- technology-choice/transaction-framework/spring-atomikos/src/main/resources/market_data_schema.sql
+++ technology-choice/transaction-framework/spring-atomikos/src/main/resources/market_data_schema.sql
@@ -2,5 +2,5 @@ DROP TABLE  MARKET_DATA_EOD_TX IF EXISTS;
 
 CREATE TABLE  MARKET_DATA_EOD_TX (
    instrument varchar(100) not null,
-   price      double not null,
+   price      double not null
 );
